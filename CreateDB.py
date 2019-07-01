5dbb67d9045673ba8a69ffd0f1092254671cd5af
Bah Me <messagetorafael@yahoo.com>
2019-06-30 19:21:57 -0500

    Updated CSV and SQLite DB with decade column. Updated loss data to reflect when actual loss ammount was given - will not affect average.

--- CreateDB.py
+++ CreateDB.py
@@ -9,12 +9,20 @@ from sqlalchemy import create_engine, func
 
 ##### import the data tornado from the CSV file
 
-tornadodataCSV = "resources/TornadoData.csv"
+##tornadodataCSV = "resources/TornadoData.csv"
+tornadodataCSV = "resources/TornadoDataWithDecades.csv"
 df = pd.read_csv(tornadodataCSV)
 
 
 #Use SQLAlchemy create_engine to [create and] connect to your sqlite database.
 engine = create_engine("sqlite:///Resources/tornadoDB.sqlite")
 
-#append the data to the database
-df.to_sql("tornadoDB", con = engine, if_exists='append', index=True)
+
+try:
+    #this will fail if there is a new column
+    #append the data to the database
+    df.to_sql(name='tornadoDB', con= engine, if_exists = 'append', index= False)
+except:
+    #this will update
+    data = pd.read_sql('SELECT * FROM tornadoDB', engine)
+    df.to_sql(name='tornadoDB', con=engine, if_exists = 'replace', index= False)
