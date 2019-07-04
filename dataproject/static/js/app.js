fdd3325e389e77f0edbec6949a1692cf1b2997a5
cluduku <44909050+cluduku@users.noreply.github.com>
2019-07-03 19:12:46 -0500

    Add files via upload

--- dataproject/static/js/app.js
+++ dataproject/static/js/app.js
@@ -1,20 +1,42 @@
-function buildMetadata(sample) {
-  console.log("Build metadata"); 
+function buildMetadata(year,state) {
+  var greenIcon = L.icon({
+    iconUrl: '../images/Tmain.png',
+    shadowUrl: '../images/Tshadow.png',
 
-  // @TODO: Complete the following function that builds the metadata panel
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+var locMarkers = [];
+  var url = `/metadata/${year}/${state}`;
+  d3.json(url).then(function(data){
+  
+    for(var i = 0; i < data.length;i++)
+    {
+      //var latLngs = [[data[i]["startingLatitude"],data[i]["startingLongitude"]],
+      //[data[i]["endingLatitude"],data[i]["endingLongitude"]]];
+      //var polyline = L.polyline(latLngs, {color: 'red'}).addTo(map);
+      locMarkers.push(
+      L.marker([data[i]["startingLatitude"],data[i]["startingLongitude"]]).bindPopup("<h2>Magnitude: </h2><hr>" + 
+      "<h3>" + data[i]["magnitude"] + "</h3>"));
+      //L.marker([data[i]["endingLatitude"],data[i]["endingLongitude"]],{icon: greenIcon}).addTo(map);
+    }
+    var locLayer = L.layerGroup(locMarkers);
 
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+    var overlayMaps = {Sites:locLayer};
+    
+    var map = new L.map("map", {
+      center: [37.0902, -95.7129],
+      zoom: 11,
+      layers:[streetLayer,locLayer]
+    });
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+  });
+  
+}//end buildMetadara
 
 function buildCharts(sample) {
   console.log("Build new chart");
@@ -29,30 +51,74 @@ function buildCharts(sample) {
 }
 
 function init() {
+  
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var yearSelector = d3.select("#selDataset");
+  var stateSelector = d3.select("#selState");
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
+    sampleNames["Years"].forEach((sample) => {
+      yearSelector
         .append("option")
-        .text(sample)
-        .property("value", sample);
+        .text(sample[0])
+        .property("value", sample[0]);
+    });
+
+    sampleNames["States"].forEach((sample) => {
+      stateSelector
+        .append("option")
+        .text(sample[0])
+        .property("value", sample[0]);
     });
 
     // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
+    //const firstSample = sampleNames[0];
+    //buildCharts(firstSample);
+    buildMetadata(sampleNames["Years"][0][0],sampleNames["States"][0][0]);
   });
 }
 
-function optionChanged(newSample) {
+function yearChanged(newYear) {
+  var curState = d3.select("#selState").property("value");
   // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+  //buildCharts(newSample);
+  buildMetadata(newYear,curState);
 }
+
+function stateChanged(newState) {
+  var curYear = d3.select("#selDataset").property("value");
+  // Fetch new data each time a new sample is selected
+  //buildCharts(newSample);
+  buildMetadata(curYear,newState);
+}
+/*
+var map = L.map("map", {
+  center: [37.0902, -95.7129],
+  zoom: 11,
+  layers:[streetLayer,locLayer]
+});
+*/
+
+// Adding tile layer
+var streetLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+
+var darkLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  Street: streetLayer,
+  Dark: darkLayer
+};
 
 // Initialize the dashboard
 init();
